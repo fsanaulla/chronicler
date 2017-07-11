c80dc2070002f64c32c6978d06a87753cfd81466
fayaz <fayaz.sanaulla@gmail.com>
2017-07-11 12:27:56 +0200

    test refactor

--- src/test/scala/com/fsanaulla/integration/InfluxDBSpec.scala
+++ src/test/scala/com/fsanaulla/integration/InfluxDBSpec.scala
@@ -1,15 +1,11 @@
 package com.fsanaulla.integration
 
 import akka.http.scaladsl.model.StatusCodes
-import com.fsanaulla.Helper._
 import com.fsanaulla.InfluxDBClient
 import com.whisk.docker.impl.spotify.DockerKitSpotify
 import com.whisk.docker.scalatest.DockerTestKit
 import org.scalatest.time.{Second, Seconds, Span}
 import org.scalatest.{FlatSpec, Matchers}
-
-import scala.concurrent.duration._
-
 
 /**
   * Created by fayaz on 06.07.17.
@@ -22,23 +18,23 @@ class InfluxDBSpec
   with DockerInfluxService {
 
   implicit val pc = PatienceConfig(Span(20, Seconds), Span(1, Second))
-  implicit val timeout: FiniteDuration = 5 second
-
   val influx = new InfluxDBClient("172.17.0.2", 8086)
 
 
   "Influxdb container" should "be ready" in {
     isContainerReady(influxdbContainer).futureValue shouldBe true
     influxdbContainer.getPorts().futureValue.get(8086) should not be empty
-    influxdbContainer.getIpAddresses().futureValue should not be Seq("")
+    influxdbContainer.getIpAddresses().futureValue should not be Seq.empty
   }
 
-  "Influxdb client" should "successfully create db" in {
-    await(influx.createDatabase("mydb")).status shouldEqual StatusCodes.OK
+  behavior of "Influxdb client"
+
+  it should "successfully create db" in {
+    influx.createDatabase("mydb").futureValue.status shouldEqual StatusCodes.OK
   }
 
-  "Influxdb client" should "successfully drop db" in {
-    await(influx.dropDatabase("mydb")).status shouldEqual StatusCodes.OK
+  it should "successfully drop db" in {
+    influx.dropDatabase("mydb").futureValue.status shouldEqual StatusCodes.OK
   }
 
 }
