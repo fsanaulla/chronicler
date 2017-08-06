e0e7981b471c08a2ecb75b52eab0baf99fb686d5
fayaz <fayaz.sanaulla@gmail.com>
2017-08-06 19:10:14 +0200

    retention policy sync api

--- src/main/scala/com/fsanaulla/api/RetentionPolicyManagement.scala
+++ src/main/scala/com/fsanaulla/api/RetentionPolicyManagement.scala
@@ -3,12 +3,35 @@ package com.fsanaulla.api
 import com.fsanaulla.InfluxClient
 import com.fsanaulla.model.Result
 import com.fsanaulla.query.RetentionPolicyManagementQuery
+import com.fsanaulla.utils.Helper._
 import com.fsanaulla.utils.ResponseWrapper.toResult
 
 import scala.concurrent.Future
 
 private[fsanaulla] trait RetentionPolicyManagement extends RetentionPolicyManagementQuery { self: InfluxClient =>
 
+  // SYNCHRONOUS API
+  def createRetentionPolicySync(rpName: String,
+                                dbName: String,
+                                duration: String,
+                                replication: Int = 1,
+                                shardDuration: Option[String] = None,
+                                default: Boolean = false): Result = {
+    await(createRetentionPolicy(rpName, dbName, duration, replication, shardDuration, default))
+  }
+
+  def updateRetentionPolicySync(rpName: String,
+                                dbName: String,
+                                duration: Option[String] = None,
+                                replication: Option[Int] = None,
+                                shardDuration: Option[String] = None,
+                                default: Boolean = false): Result = {
+    await(updateRetentionPolicy(rpName, dbName, duration, replication, shardDuration, default))
+  }
+
+  def dropRetentionPolicySync(rpName: String, dbName: String): Result = await(dropRetentionPolicy(rpName, dbName))
+
+  //ASYNCHRONOUS API
   def createRetentionPolicy(rpName: String,
                             dbName: String,
                             duration: String,
