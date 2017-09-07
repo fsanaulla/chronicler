78b226c278ef4c099504f03c093a4d943531dbed
fayaz <fayaz.sanaulla@gmail.com>
2017-09-07 12:55:20 +0300

    system api spec

---
+++ src/test/scala/com/github/fsanaulla/integration/SystemApiSpec.scala
@@ -0,0 +1,26 @@
+package com.github.fsanaulla.integration
+
+import akka.actor.Terminated
+import com.github.fsanaulla.InfluxClientsFactory
+import com.github.fsanaulla.utils.TestHelper._
+import com.github.fsanaulla.utils.TestSpec
+
+/**
+  * Created by
+  * Author: fayaz.sanaulla@gmail.com
+  * Date: 07.09.17
+  */
+class SystemApiSpec extends TestSpec {
+
+  "System api" should "correctly work" in {
+    val influx = InfluxClientsFactory.createHttpClient(
+      influxHost,
+      username = credentials.username,
+      password = credentials.password)
+
+    influx.ping().futureValue shouldEqual OkResult
+
+    influx.close().futureValue shouldEqual Terminated
+  }
+
+}
