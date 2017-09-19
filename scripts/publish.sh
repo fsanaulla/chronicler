1ac55cf415892440a0b61a7c9de481fc88b72396
fayaz <fayaz.sanaulla@gmail.com>
2017-09-19 12:48:36 +0300

    publish sh fix

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-sbt
-pgp-cmd recv-key ${PGP_KEY} hkp://pool.sks-keyservers.net
-release release-version ${RELEASE_VERSION} next-version ${NEXT_RELEASE_VERSION}
+sbt "pgp-cmd recv-key ${PGP_KEY} hkp://pool.sks-keyservers.net"
+sbt release release-version ${RELEASE_VERSION} next-version ${NEXT_RELEASE_VERSION}
