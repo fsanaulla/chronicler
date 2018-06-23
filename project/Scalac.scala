b73df8aea3cdf1a1415bc77f2fce2e6a03cdedf1
fsanaulla <fayaz.sanaulla@gmail.com>
2018-06-24 00:16:12 +0300

    [SBT][ISSUE-65]: fixing scalac options

--- project/Scalac.scala
+++ project/Scalac.scala
@@ -5,22 +5,21 @@ object Scalac {
     case _ => options11
   }
 
-  // Warn if an implicit parameter is unused.
-  private val unusedImplicits = "-Ywarn-unused:implicits"
-
   // scalac 2.11 options
   private val options11 = Seq(
     "-deprecation",
     "-feature",
-    "-encoding", "utf-8",       // Specify character encoding used by source files.
+    "-encoding", "utf-8"       // Specify character encoding used by source files.
+  )
+
+  // scalac 2.12 options
+  private val options12: Seq[String] = Seq(
     "-Ywarn-unused:imports",    // Warn if an import selector is not referenced.
     "-Ywarn-unused:locals",     // Warn if a local definition is unused.
     "-Ywarn-unused:params",     // Warn if a value parameter is unused.
     "-Ywarn-unused:patvars",    // Warn if a variable bound in a pattern is unused.
-    "-Ywarn-unused:privates"    // Warn if a private member is unused.
-//    "-Xfatal-warnings"
-  )
-
-  // scalac 2.12 options
-  private val options12: Seq[String] = unusedImplicits +: options11
+    "-Ywarn-unused:privates",   // Warn if a private member is unused.
+//    "-Xfatal-warnings",
+    "-Ywarn-unused:implicits"   // Warn if an implicit parameter is unused.
+  ) ++ options11
 }
