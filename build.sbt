69c1dc36e529c0142c46fbe78ebdbebf158b9610
fsanaulla <fayaz.sanaulla@gmail.com>
2018-06-23 21:31:07 +0300

    [SBT][ISSUE-65]: configuring scalac options

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ import sbt.url
 lazy val commonSettings = Seq(
   scalaVersion := "2.12.6",
   organization := "com.github.fsanaulla",
-  scalacOptions ++= Scalac.options,
+  scalacOptions ++= Scalac.options(scalaVersion.value),
   crossScalaVersions := Seq("2.11.8", scalaVersion.value),
   homepage := Some(url("https://github.com/fsanaulla/chronicler")),
   licenses += "Apache-2.0" -> url("https://opensource.org/licenses/Apache-2.0"),
--- project/Scalac.scala
+++ project/Scalac.scala
@@ -1,14 +1,26 @@
 object Scalac {
 
-  val options = Seq(
+  def options(version: String): Seq[String] = version match {
+    case v if v.startsWith("2.12") => options12
+    case _ => options11
+  }
+
+  // Warn if an implicit parameter is unused.
+  private val unusedImplicits = "-Ywarn-unused:implicits"
+
+  // scalac 2.11 options
+  private val options11 = Seq(
     "-deprecation",
     "-feature",
     "-encoding", "utf-8",       // Specify character encoding used by source files.
-    "-Ywarn-unused:implicits",  // Warn if an implicit parameter is unused.
     "-Ywarn-unused:imports",    // Warn if an import selector is not referenced.
     "-Ywarn-unused:locals",     // Warn if a local definition is unused.
     "-Ywarn-unused:params",     // Warn if a value parameter is unused.
     "-Ywarn-unused:patvars",    // Warn if a variable bound in a pattern is unused.
     "-Ywarn-unused:privates"    // Warn if a private member is unused.
+//    "-Xfatal-warnings"
   )
+
+  // scalac 2.12 options
+  private val options12: Seq[String] = unusedImplicits +: options11
 }
