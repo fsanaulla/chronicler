ff33b52488fbedfa89e78058ed851637eb29986e
fsanaulla <fayaz.sanaulla@gmail.com>
2018-12-25 18:51:13 +0200

    [CI][ISSUE-112] - fix module name

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-sbt ++${TRAVIS_SCALA_VERSION} corePublish
-sbt ++${TRAVIS_SCALA_VERSION} akkaPublish
-sbt ++${TRAVIS_SCALA_VERSION} ahcPublish
-sbt ++${TRAVIS_SCALA_VERSION} urlPublish
-sbt ++${TRAVIS_SCALA_VERSION} macrosPublish
-sbt ++${TRAVIS_SCALA_VERSION} udpPublish
+modules=( coreShared coreIO coreManagement akkaShared akkaIO akkaManagement )
+
+for md in "${modules[@]}"
+do
+   sbt "project $md" "+ fullRelease"
+done
+#sbt ++${TRAVIS_SCALA_VERSION} corePublish
+#sbt ++${TRAVIS_SCALA_VERSION} akkaPublish
+#sbt ++${TRAVIS_SCALA_VERSION} ahcPublish
+#sbt ++${TRAVIS_SCALA_VERSION} urlPublish
+#sbt ++${TRAVIS_SCALA_VERSION} macrosPublish
+#sbt ++${TRAVIS_SCALA_VERSION} udpPublish
