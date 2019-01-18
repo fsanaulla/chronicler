e003cc84f41f765d88bf42bff1b1dd0964ec6d04
fsanaulla <fayaz.sanaulla@gmail.com>
2019-01-19 00:38:33 +0200

    ISSUE-119: db version

--- testing/it/src/main/scala/com/github/fsanaulla/chronicler/testing/it/DockerizedInfluxDB.scala
+++ testing/it/src/main/scala/com/github/fsanaulla/chronicler/testing/it/DockerizedInfluxDB.scala
@@ -6,7 +6,7 @@ import org.testcontainers.containers.InfluxDBContainer
 
 trait DockerizedInfluxDB extends BeforeAndAfterAll { self: Suite =>
 
-  private val influx = new InfluxDBContainer()
+  private val influx = new InfluxDBContainer(sys.env.getOrElse("INFLUXDB_VERSION", "1.7.3"))
 
   /** Credentials for influx */
   final val creds: InfluxCredentials = InfluxCredentials("admin", "password")
