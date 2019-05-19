e4d79b4fb58635322f2b83947b561faaea00d1fc
fsanaulla <fayaz.sanaulla@gmail.com>
2019-05-19 15:07:11 +0300

    [CI][ISSUE-119]: adding git remote while releasing

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 # set github user info
-git config --local user.name ${GITHUB_USERNAME}
-git config --local user.email ${GITHUB_EMAIL}
+git config --local user.email "travis@travis-ci.org"
+git config --local user.name "Travis CI"
+git remote add origin https://${GITHUB_TOKEN}@github.com/fsanaulla/chronicler.git
 
 # get tag from version.sbt
 version="$(sed -n 's/.*version in ThisBuild := \"\([0-9].[0-9].[0-9]*\)\"/\1/p' version.sbt)" # extracted version
 tag="v$version" # git tag
+
+echo $tag
 
 # create tag
 git tag ${tag}
