596d4f954747f635348f1d7ecd1b607ee3a67e2e
fsanaulla <fayaz.sanaulla@gmail.com>
2019-05-19 12:21:23 +0300

    [CI][ISSUE-119]: configure travis ci tagging

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+# set github user info
+git config --local user.name ${GITHUB_USERNAME}
+git config --local user.email ${GITHUB_EMAIL}
+
+# get tag from version.sbt
 version="$(sed -n 's/.*version in ThisBuild := \"\([0-9].[0-9].[0-9]*\)\"/\1/p' version.sbt)" # extracted version
 tag="v$version" # git tag
-git tag ${tag} # create git tag
-git push origin ${tag} # push git tag to remote repo
+
+# create tag
+git tag ${tag}
+
+# push git tag to remote repo
+git push origin ${tag}
+
+# push release
 sbt githubRelease
