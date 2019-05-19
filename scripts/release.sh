1fc6c6994b7cd0e0c7a91cf577f90dd025b025fe
fsanaulla <fayaz.sanaulla@gmail.com>
2019-05-19 15:57:09 +0300

    [CI][ISSUE-119]: trying to fix auth problem

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# set github user info
-git config --local user.email "travis@travis-ci.org"
-git config --local user.name "Travis CI"
-git remote add origin https://${GITHUB_TOKEN}@github.com/fsanaulla/chronicler.git
-
 # get tag from version.sbt
 version="$(sed -n 's/.*version in ThisBuild := \"\([0-9].[0-9].[0-9]*\)\"/\1/p' version.sbt)" # extracted version
 tag="v$version" # git tag
@@ -15,7 +10,4 @@ echo $tag
 git tag ${tag}
 
 # push git tag to remote repo
-git push origin ${tag}
-
-# push release
-sbt githubRelease
+git push https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/fsanaulla/chronicler.git ${tag}
