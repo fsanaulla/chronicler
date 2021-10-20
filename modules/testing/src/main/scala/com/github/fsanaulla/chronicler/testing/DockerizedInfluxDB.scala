2c856ea7c64b0612c1cb9f8daa0d0b94f1e8e887
fsanaulla <fayaz.sanaulla@gmail.com>
2021-10-20 13:51:57 +0300

    update testing module

--- modules/testing/src/main/scala/com/github/fsanaulla/chronicler/testing/DockerizedInfluxDB.scala
+++ modules/testing/src/main/scala/com/github/fsanaulla/chronicler/testing/DockerizedInfluxDB.scala
@@ -1,16 +1,16 @@
 package com.github.fsanaulla.chronicler.testing.it
 
 import com.dimafeng.testcontainers.{ForAllTestContainer, GenericContainer}
-import com.github.fsanaulla.chronicler.core.model.InfluxCredentials
+import com.github.fsanaulla.chronicler.core.auth.InfluxCredentials
 import org.scalatest.Suite
 import org.testcontainers.containers.output.OutputFrame.OutputType
 import org.testcontainers.containers.output.ToStringConsumer
 import org.testcontainers.containers.wait.strategy.Wait
 
 trait DockerizedInfluxDB extends ForAllTestContainer { self: Suite =>
-  def adminName = "admin"
-  def adminPassword = "password"
-  def influxPort        = 8086
+  def adminName       = "admin"
+  def adminPassword   = "password"
+  def influxPort      = 8086
   def version: String = sys.env.getOrElse("INFLUXDB_VERSION", "1.7.3")
 
   override val container: GenericContainer =
@@ -19,14 +19,14 @@ trait DockerizedInfluxDB extends ForAllTestContainer { self: Suite =>
       exposedPorts = Seq(influxPort),
       waitStrategy = Wait.forHttp("/ping").forStatusCode(204),
       env = Map(
-        "INFLUXDB_ADMIN_USER"     -> adminName,
-        "INFLUXDB_ADMIN_PASSWORD" -> adminPassword,
+        "INFLUXDB_ADMIN_USER"        -> adminName,
+        "INFLUXDB_ADMIN_PASSWORD"    -> adminPassword,
         "INFLUXDB_HTTP_AUTH_ENABLED" -> String.valueOf(true)
       )
     )
 
   /** Credentials for influx */
-  final val credentials: InfluxCredentials = InfluxCredentials(adminName, adminPassword)
+  final val credentials: InfluxCredentials = InfluxCredentials.Basic(adminName, adminPassword)
 
   /** host address */
   def host: String = container.container.getContainerIpAddress
